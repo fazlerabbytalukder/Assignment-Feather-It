403b1c91c89e23fec307e9b04c33167596ed2110
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-11-27 22:52:49 -0800

    social site link

--- src/components/SocialSites/SocialSites.jsx
+++ src/components/SocialSites/SocialSites.jsx
@@ -5,18 +5,26 @@ import { FaFacebookF } from "react-icons/fa";
 const SocialSites = () => {
     return (
         <div>
-            <div className="insta bg-secondary rounded-full p-1 mb-2">
-                <BsInstagram />
-            </div>
-            <div className="facebook bg-secondary rounded-full p-1 mb-2">
-                <FaFacebookF />
-            </div>
-            <div className="twitter bg-secondary rounded-full p-1 mb-2">
-                <BsTwitter />
-            </div>
-            <div className="whatapp bg-secondary rounded-full p-1 mb-2">
-                <BsWhatsapp />
-            </div>
+            <a className='cursor-pointer' href="/">
+                <div className="insta bg-secondary rounded-full p-1 mb-2">
+                    <BsInstagram />
+                </div>
+            </a>
+            <a className='cursor-pointer' href="/">
+                <div className="facebook bg-secondary rounded-full p-1 mb-2">
+                    <FaFacebookF />
+                </div>
+            </a>
+            <a className='cursor-pointer' href="/">
+                <div className="twitter bg-secondary rounded-full p-1 mb-2">
+                    <BsTwitter />
+                </div>
+            </a>
+            <a className='cursor-pointer' href="/">
+                <div className="whatapp bg-secondary rounded-full p-1 mb-2">
+                    <BsWhatsapp />
+                </div>
+            </a>
         </div>
     );
 };
